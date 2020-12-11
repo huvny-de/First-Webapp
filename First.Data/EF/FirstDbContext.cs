f35912ac3d35db0413606749f6426f532f78c144
Huy Vo <neo.pinhuy00@gmail.com>
2020-12-12 04:02:11 +0700

    Add Entities & Entity FrameWork

---
+++ First.Data/EF/FirstDbContext.cs
@@ -0,0 +1,17 @@
+﻿using First.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace First.Data.EF
+{
+    public class FirstDbContext : DbContext
+    {
+        public FirstDbContext( DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { set; get; }
+        public DbSet<Category> Products { set; get; }
+    }
+}
